c8133beb07e8d86fe20b963ab017bdc1bc78515b
Emmanuel Onyeyaforo <rahzy24@gmail.com>
2023-02-07 13:44:42 +0100

    update

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,155 @@
 const express = require('express');
 const router = express.Router();
-
 const { register, login, verifyEmail, verifyOTP, resendOTP } = require('../controllers/auth');
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     RegisterDTO:
+ *       type: object
+ *       required:
+ *         - username
+ *         - email
+ *         - password
+ *       properties:
+ *         username:
+ *           type: string
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     LoginDTO:
+ *       type: object
+ *       required:
+ *         - email
+ *         - password
+ *       properties:
+ *         email:
+ *           type: string
+ *         password:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Verify2FADTO:
+ *       type: object
+ *       required:
+ *         - otp
+ *       properties:
+ *         otp:
+ *           type: string
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     ResendOTP_DTO:
+ *       type: object
+ *       required:
+ *         - otp
+ */
+
+
+/**
+  * @swagger
+  * tags:
+  *   name: User Auth
+  */
+
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: create a new user
+ *     tags: [User Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/RegisterDTO'
+ *     responses:
+ *       200:
+ *         description: user created successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/RegisterDTO'
+ *       500:
+ *         description: Some server error
+ */
+
+/**
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     tags: [User Auth]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/LoginDTO'
+ *     responses:
+ *       200:
+ *         description: 
+ */
+
+/**
+ * @swagger
+ * /verify2FA/{id}:
+ *  post:
+ *    tags: [User Auth]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Verify2FADTO'
+ *    responses:
+ *      200:
+ *        description: 
+ */
+
+/**
+ * @swagger
+ * /resend-otp/{id}:
+ *  post:
+ *    tags: [User Auth]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/ResendOTP_DTO'
+ *    responses:
+ *      200:
+ *        description: 
+ */
+
 
 
 router.route("/verify2FA/:id").post(verifyOTP)
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const connectDB = require('./config/db');
 const errorHandler = require('./middlewares/error')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const swaggerUI = require("swagger-ui-express");
+const swaggerJsDoc = require("swagger-jsdoc");
 
 
 // Connect DB
@@ -13,6 +15,26 @@ app.use(express.json());
 
 app.use(bodyParser.json());
 app.use(cors());
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Arctic Travels API",
+      version: "1.0.0",
+      description: "Test/Production API for artic travels",
+    },
+    servers: [
+      { url: "http://localhost:4000" },
+      { url: "https://arctic-travels-api.cyclic.app/", }
+    ],
+  },
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 
 
 // authentication routes
@@ -33,8 +55,8 @@ const PORT = process.env.PORT || 4000
 const server = app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 
 process.on("unhandledRejections", (err, promise) => {
-    console.log(`Logged Error: ${err}`);
-    server.close(() => process.exit(1));
+  console.log(`Logged Error: ${err}`);
+  server.close(() => process.exit(1));
 });
 
 
